136c6fdd7f0e351f020797c5825e874751bcfe60
Павел <115927793+Pawel070@users.noreply.github.com>
2023-08-16 10:29:40 +0300

    ShaeIt ТЗ 16 gateway server test-2 корректировка 1 92проц + postman ok-8

--- server/src/test/java/ru.practicum.shareit/booking/service/BookingServiceImplTest.java
+++ server/src/test/java/ru.practicum.shareit/booking/service/BookingServiceImplTest.java
@@ -33,7 +33,6 @@ import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.expections.EntityNotAvailable;
 import ru.practicum.shareit.expections.NotFoundException;
-import ru.practicum.shareit.expections.UnsupportedState;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.service.ItemRepository;
